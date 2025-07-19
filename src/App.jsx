781300d73a11eee1d00ce9e367772baac638de30
KoketsoPlus <kchabalala1105@gmail.com>
2025-07-19 15:25:35 +0200

    completed hero section of the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import HeroSection from './components/HeroSection';
 import PartnerLogos from './components/PartnerLogos';
+import Footer from './components/Footer';
+import './src/App.css';
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <HeroSection />
+
+      {/* Optional Spacer */}
+      <div style={{ height: '10vh' }}></div>
+
       <PartnerLogos />
-    </>
+
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
 
