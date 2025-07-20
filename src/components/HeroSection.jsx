1b65b41f5637d60c4a949f4ba6a984c42e4729e0
KoketsoPlus <kchabalala1105@gmail.com>
2025-07-20 11:17:54 +0200

    imported missing css from PartnerLogos.css

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './PartnerLogos.css';
+
 
 const HeroSection = () => {
   return (
--- src/components/MainConetent.jsx
+++ src/components/MainConetent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PartnerLogos.css';
 
 const HeroSection = () => {
   return (
